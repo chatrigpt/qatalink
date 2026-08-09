1419a165a02293bf02f5a5701614d8bde49d3df9
chatrigpt <145403724+chatrigpt@users.noreply.github.com>
2026-08-09 23:36:59 +0000

    Add Meta Pixel tracking

---
+++ components/meta-pixel.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Script from 'next/script';
+import {usePathname,useSearchParams} from 'next/navigation';
+import {useEffect,useRef} from 'react';
+
+declare global { interface Window { fbq?: (...args:any[])=>void; _fbq?: unknown; } }
+
+const PIXEL_ID='1350948426695753';
+
+export function MetaPixel(){
+  const pathname=usePathname();
+  const search=useSearchParams();
+  const first=useRef(true);
+
+  useEffect(()=>{
+    if(first.current){first.current=false;return}
+    window.fbq?.('track','PageView');
+  },[pathname,search]);
+
+  return <>
+    <Script id="meta-pixel" strategy="afterInteractive">{`
+      !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+      (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init','${PIXEL_ID}');fbq('track','PageView');
+    `}</Script>
+    <noscript><img height="1" width="1" style={{display:'none'}} src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`} alt=""/></noscript>
+  </>;
+}
