62e0d544929a3d5a6525db5effe6803b3ecbe8f2
chatrigpt <145403724+chatrigpt@users.noreply.github.com>
2026-08-28 10:34:04 +0000

    TEMP enable PWA Bluetooth in team POS

---
+++ app/ops/[key]/layout.tsx
@@ -0,0 +1,2 @@
+import {PwaBluetoothPrinterBridge} from '@/components/pwa-bluetooth-printer-bridge';
+export default function OpsLayout({children}:{children:React.ReactNode}){return <>{children}<PwaBluetoothPrinterBridge/></>}
